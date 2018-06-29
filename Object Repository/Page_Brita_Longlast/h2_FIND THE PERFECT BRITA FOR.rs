0fd21ced38971e58752709c982e7a7111cf8df35
smaniiiiyer <smaniiyer@triadretail.com>
2018-06-29 15:53:49 +0530

    all new codes

---
+++ Object Repository/Page_Brita_Longlast/h2_FIND THE PERFECT BRITA FOR.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_FIND THE PERFECT BRITA FOR</name>
+   <tag></tag>
+   <elementGuidId>451326f7-3c0d-4704-90ac-b9c54bc02c99</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>FIND THE PERFECT BRITA® FOR YOU</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ContentPlaceHolder1_C001_divContent&quot;)/div[@class=&quot;trmBodyContent&quot;]/div[@class=&quot;trm-pagewrap&quot;]/div[@class=&quot;trm-container&quot;]/div[@class=&quot;trm-products-section&quot;]/div[@class=&quot;trm-page-title&quot;]/h2[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/all.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>all</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-29T15:30:38</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8646bb66-1ae9-4034-99a9-6946f50e10ac</testSuiteGuid>
+   <testCaseLink>
+      <guid>179ab22c-d37e-4abb-8f87-18de747d983e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/regg</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
