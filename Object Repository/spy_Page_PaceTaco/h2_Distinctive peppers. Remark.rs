b937fbf2c47e463fcc38b8cc263cbba92f3ace80
smaniiiiyer <smaniiyer@triadretail.com>
2018-07-19 19:36:38 +0530

    01_CAM_R6792 code

--- Object Repository/spy_Page_PaceTaco/h2_Distinctive peppers. Remark.rs
+++ Object Repository/spy_Page_PaceTaco/h2_Distinctive peppers. Remark.rs
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>h2_Shop the IdeaPad 330S</name>
+   <name>h2_Distinctive peppers. Remark</name>
    <tag></tag>
-   <elementGuidId>e9b5ef27-2614-49b0-8104-f5ed8b592393</elementGuidId>
+   <elementGuidId>df6fce32-e209-47fb-956d-50f07c9603a2</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
@@ -18,13 +18,13 @@
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
-      <value>Shop the IdeaPad 330S</value>
+      <value>Distinctive peppers. Remarkable flavors.</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;ContentPlaceHolder1_C001_divContent&quot;)/div[@class=&quot;trmBodyContent&quot;]/main[@class=&quot;trm-main&quot;]/section[@class=&quot;trm-products&quot;]/h2[1]</value>
+      <value>id(&quot;ContentPlaceHolder1_C001_divContent&quot;)/div[@class=&quot;trmBodyContent&quot;]/div[@class=&quot;trm-pagewrap&quot;]/div[@class=&quot;trm-container&quot;]/div[@class=&quot;trm-flavor-section Grid&quot;]/div[@class=&quot;trm-section-title&quot;]/h2[1]</value>
    </webElementProperties>
 </WebElementEntity>
--- Object Repository/spy_Page_PaceTaco/h2_Throw a fiesta.rs
+++ Object Repository/spy_Page_PaceTaco/h2_Throw a fiesta.rs
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>h2_More efficient</name>
+   <name>h2_Throw a fiesta</name>
    <tag></tag>
-   <elementGuidId>b4e25ea5-6e07-4c56-8fb2-b53506308733</elementGuidId>
+   <elementGuidId>e3a59466-03d3-44b7-8c24-c95ecb38e4d1</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
@@ -18,13 +18,13 @@
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
-      <value>More efficient</value>
+      <value>Throw a fiesta</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;ContentPlaceHolder1_C001_divContent&quot;)/div[@class=&quot;trmBodyContent&quot;]/main[@class=&quot;trm-main&quot;]/section[@class=&quot;trm-efficient&quot;]/h2[1]</value>
+      <value>id(&quot;ContentPlaceHolder1_C001_divContent&quot;)/div[@class=&quot;trmBodyContent&quot;]/div[@class=&quot;trm-pagewrap&quot;]/div[@class=&quot;trm-container&quot;]/div[@class=&quot;trm-mod Grid&quot;]/div[@class=&quot;Grid-col u-size-1-2-m u-size-1-2-sm trm-mod-content&quot;]/h2[1]</value>
    </webElementProperties>
 </WebElementEntity>
