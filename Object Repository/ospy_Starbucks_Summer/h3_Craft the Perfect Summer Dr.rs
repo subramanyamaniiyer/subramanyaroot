2bd427f546927ddd806e1abd613527c8f688ce94
smaniiiiyer <smaniiyer@triadretail.com>
2018-07-23 21:52:09 +0530

    01_PEP_Q0194_StarbucksSum

---
+++ Object Repository/ospy_Starbucks_Summer/h3_Craft the Perfect Summer Dr.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Craft the Perfect Summer Dr</name>
+   <tag></tag>
+   <elementGuidId>4e1d8e05-d364-446d-bc70-418cea8e939b</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Craft the Perfect Summer Drinks</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ContentPlaceHolder1_C001_divContent&quot;)/div[@class=&quot;trmBodyContent&quot;]/main[@class=&quot;trm-pagewrap&quot;]/div[@class=&quot;trm-container&quot;]/div[@class=&quot;trm-starbucks-scrollwrap&quot;]/h3[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/regsuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>regsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>81fca0f6-1163-47ea-82cd-a1611eff9e0b</testSuiteGuid>
+   <testCaseLink>
+      <guid>e04af1b2-b4a5-4f8c-bc59-ef88b01bd31c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/regg</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
