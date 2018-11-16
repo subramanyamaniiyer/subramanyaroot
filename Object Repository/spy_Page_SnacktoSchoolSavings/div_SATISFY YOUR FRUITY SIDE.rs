cbf4211a2ab48408c24c77d7af959b3bdf421a9f
smaniiiiyer <smaniiyer@triadretail.com>
2018-11-16 16:59:23 +0530

    25_DAN_R9206

---
+++ Object Repository/spy_Page_SnacktoSchoolSavings/div_SATISFY YOUR FRUITY SIDE.rs
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_SATISFY YOUR FRUITY SIDE</name>
+   <tag></tag>
+   <elementGuidId>49218c01-4568-401f-84f1-43841d2b5b2e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>trm-btm-wrap</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                
+                    
+                    
+                        SATISFY YOUR FRUITY SIDE
+                        
+                            GOOD SOURCE OF CALCIUM &amp; VITAMIN D
+                            NO ARTIFICIAL FLAVORS
+                            DELICIOUS TASTE KIDS LOVE
+                        
+                    
+                    
+                        
+                        
+                        
+                    
+                    
+                
+                
+                    
+                    
+                        
+                        SHAKE UP YOUR SWEET SIDE
+                        
+                            LOW-FAT YOGURT
+                            SATISFYING CRUNCH
+                            PERFECT FOR ON-THE-GO SNACKING FUN
+                        
+                    
+                    
+                        
+                        
+                    
+                
+            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main&quot;)/div[@class=&quot;container-fluid trm-no-paddin&quot;]/div[@class=&quot;trm-wrap&quot;]/div[@class=&quot;trm-btm-wrap&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/spy_Page_SnacktoSchoolSavings/div_SNACK-TO-SCHOOL SAVINGS.rs
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_SNACK-TO-SCHOOL SAVINGS</name>
+   <tag></tag>
+   <elementGuidId>d4d573a9-69b3-43db-9112-263ccc593537</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>trm-pov-wrap</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                
+                    SNACK-TO-SCHOOL SAVINGS
+                    Save $1
+                    
+                        on any 1 Danimals® multipack or any 3 YoCrunch® singles with
+                        
+                    
+                    Offer valid Tues, 8/7 – Tues, 9/4. 
+                    
+                
+            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main&quot;)/div[@class=&quot;container-fluid trm-no-paddin&quot;]/div[@class=&quot;trm-wrap&quot;]/div[@class=&quot;trm-pov-wrap&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
