5f916373e44927f1f3bc34337fda1e6fc44c12f7
smaniiiiyer <smaniiyer@triadretail.com>
2018-11-19 19:15:10 +0530

    01_KFT_R3374

---
+++ Object Repository/spy_Page_JustCrackanEgg/div_Just crack an egg.rs
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Just crack an egg</name>
+   <tag></tag>
+   <elementGuidId>75477e8a-84bc-4515-a532-71a258bb228f</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>trm-pov-card</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        Just crack an egg™
+                        Find it in the egg aisle.
+                        
+                            
+                            
+                        
+                    </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ContentPlaceHolder1_C001_divContent&quot;)/div[@class=&quot;trmBodyContent&quot;]/div[@class=&quot;trm-pagewrap&quot;]/div[@class=&quot;trm-container&quot;]/div[@class=&quot;trm-pov-module&quot;]/div[@class=&quot;trm-pov-slider&quot;]/div[@class=&quot;trm-pov-slide&quot;]/div[@class=&quot;trm-pov-card&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
